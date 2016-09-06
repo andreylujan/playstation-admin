06efeecba8b719e60b347e5e73d8c64642a57c8d
Álvaro Troncoso <alvaro.mc2@gmail.com>
2016-09-06 20:42:25 -0300

    change url production

--- app/scripts/services/network.js
+++ app/scripts/services/network.js
@@ -1,14 +1,14 @@
 'use strict';
 
 
-// var API_URL = 'http://10.77.0.189:3001/api/v1'; // PRODUCCIÓN
-// var URL_SERVER = 'http://10.77.0.189:3001/'; // PRODUCCIÓN
+var API_URL = 'http://50.16.161.152/eretail/api/v1'; // PRODUCCIÓN
+var URL_SERVER = 'http://50.16.161.152/'; // PRODUCCIÓN
 
 // var API_URL = 'http://192.168.1.184:3000/api/v1'; // DESARROLLO PABLO
 // var URL_SERVER = 'http://192.168.1.184:3000/'; // DESARROLLO PABLO
 
-var API_URL = 'http://50.16.161.152/eretail-staging/api/v1'; // DESARROLLO
-var URL_SERVER = 'http://50.16.161.152/eretail-staging'; // DESARROLLO
+// var API_URL = 'http://50.16.161.152/eretail-staging/api/v1'; // DESARROLLO
+// var URL_SERVER = 'http://50.16.161.152/eretail-staging'; // DESARROLLO
 
 angular.module('minovateApp')
 
